23c9499bed27ab4e45a4987c31a484a505c1a690
tuanminhphan45 <tuanminhphan45@gmail.com>
2024-08-30 23:25:54 +0700

    đổ data

--- product-management/controllers/admin/products.controller.js
+++ product-management/controllers/admin/products.controller.js
@@ -1,6 +1,13 @@
+const Product = require("../../model/product.model");
+
 // [GET] /admin/products
 module.exports.products = async (req, res) => {
+    const products = await Product.find({
+        deleted: false,
+    });
+    console.log(products);
     res.render("admin/pages/products/index", {
         pageTitle: "Trang chủ",
+        products: products
     });
-}
+};
