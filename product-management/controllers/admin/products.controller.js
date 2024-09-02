00a522b66d6dbd1221bba5e8972ad494807e9158
tuanminhphan45 <tuanminhphan45@gmail.com>
2024-09-02 22:12:20 +0700

    tính năng lọc theo trạng thái products

--- product-management/controllers/admin/products.controller.js
+++ product-management/controllers/admin/products.controller.js
@@ -2,11 +2,34 @@ const Product = require("../../model/product.model");
 
 // [GET] /admin/products
 module.exports.products = async (req, res) => {
+    const filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: "active",
+        },
+        {
+            name: "Hoạt Động",
+            status: "active",
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            
+        },
+    ];
     const find = {
         deleted: false,
     };
     if (req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus.class = "active"
         find.status = req.query.status;
+    }else{
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus.class = "active"
+        find.status = req.query.status;
+
     }
 
     const products = await Product.find(find);
@@ -14,5 +37,6 @@ module.exports.products = async (req, res) => {
     res.render("admin/pages/products/index", {
         pageTitle: "Trang chủ",
         products: products,
+        filterStatus: filterStatus,
     });
 };
