ea2d65d890e749159ca21b5d6169ebe4ded6dd5b
Laptop <tuanminhphan45@gmail.com>
2025-02-06 14:31:11 +0700

    thêm thời gian xóa sản phẩm lưu trữ trong database

--- 28tech/controllers/admin/product.controller.js
+++ 28tech/controllers/admin/product.controller.js
@@ -80,6 +80,8 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.delete = async (req, res) => {
     const id = req.params.id;
     console.log(id);
-    await Product.deleteOne({ _id: id });
+    await Product.updateOne({ _id: id }, { deleted: true, 
+        deleteAt: new Date()
+    });
     res.redirect("back");
 };
